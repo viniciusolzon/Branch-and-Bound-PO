1715586c97c6df3a0d76a709027d42f60ad577b5
Vinicius Freitas <91697213+viniciusolzon@users.noreply.github.com>
2023-05-28 14:31:41 -0300

    quase la

--- src/bnb.py
+++ src/bnb.py
@@ -1,18 +1,134 @@
-class Node():
-    def __init__(self):
-        self.lower_bound = 0
-        self.objValue
-        self.choice = 2 # por padrão executaremos a busca em largura (Breadth First Search)
+# importação do pacote mip
+from mip import *
+import numpy as np
+import sys
 
-    def prune(self):
-        if True:
+# salva modelo em arquivo lp, e mostra o conteúdo
+def save(model, filename):
+    model.write(filename) # salva modelo em arquivo
+    with open(filename, "r") as f: # lê e exibe conteúdo do arquivo
+        print(f.read())
+
+
+def Integer(variable):
+    if variable - int(variable) == 0:
+        return True
+    return False
+
+
+def proximity(variable):
+    # print(f"Variable = {variable}")
+    return abs(variable - 0.5)
+
+
+def getBranchVariableIndex(variable_value_list):
+        # tira todos os valores inteiros e de valor 0 (temporariamente) da lista de valores das variaveis
+        value_list = [i for i in variable_value_list if i != 0 or not Integer(i)]
+        # escolhe a variavel fracionaria mais proxima de 0,5
+        branch_variable = min(value_list, key=proximity)
+        return variable_value_list.index(branch_variable) # caso tenha mais de uma variável com o mesmo valor estamos escolhendo a de menor índice
+
+
+class Node():
+    def __init__(self, model):
+        self.model = model
+        self.cost = float("inf")
+
+    def integralSolution(self):
+        variable_value_list = self.getVariableValueList()
+        non_integer_list = [i for i in variable_value_list if i != 0 or not Integer(i)]
+        if len(non_integer_list) == 0:
+            # print("Poda por integralidade")
             return True
-        else:
-            return False
+
+    def isInfeasible(self):
+        status = self.model.optimize()
+        if status == OptimizationStatus.INFEASIBLE:
+            print("Solucao e inviavel")
+            return True
+        return False
+
+    def toPrune(self, lb):
+        # INVIABILIDADE
+        # se a solução for inviavels podamos
+        if self.isInfeasible():
+            return True
+        print("Solucao e viavel")
+       
+        # INTEGRALIDADE
+        # se a solução for inteira podamos
+        if self.integralSolution():
+            return True
+        print("Solucao nao e inteira")
+    
+        # LIMITANTE
+        # se a solução for inteira e possuir um valor menor do que o lower bound atual podamos
+        if self.integralSolution() and self.model.objective_value <= lb:
+            # print("Poda por limitante")
+            return True
+        print("Solucao nao e menor que lower bound")
+
+    def getVariableValueList(self):
+        variable_value_list = []
+        for i in range(len(self.model.vars)):
+            variable_value_list.append(self.model.var_by_name(f"x_{i+1}").x)
+        return variable_value_list
 
     def solve(self):
-        print("\tBranch and Bound\n\nEscolha o método de exploração da árvore:")
-        print("(1) Busca em profundidade - Depth First Search")
-        print("(2) Busca em largura      - Breadth First Search")
-        self.choice = int(input("-> "))
-        pass
+        status = self.model.optimize()
+
+        if status != OptimizationStatus.OPTIMAL:
+            return
+
+        print("Status = ", status)
+        print(f"Solution value  = {self.model.objective_value:.2f}\n")
+        
+        print("Solution:")
+        for v in self.model.vars:
+            print(f"{v.name} = {v.x:.2f}")
+        print("###########################################################")
+
+
+def solveProblem(baseModel):
+
+    root = Node(baseModel)
+    best_node = Node(baseModel)
+    best_node.cost = 0 # mesma coisa que o lower_bound ou limite primal
+    
+    tree = []
+    tree.append(root)
+
+    # branch and bound
+    while len(tree) != 0:
+        node = tree[-1] # estrategia de busca em profundidade, vamos pelo ultimo no inserido na lista de nos (DFS)
+        node.solve()
+        # save(node.model, "modelo1.lp")
+        
+        # se satisfazer algum dos tres criterios de poda, ele poda o no atual (tira ele da lista de nos)
+        if not node.toPrune(best_node.model.objective_value):
+            # se a solução for inteira e possuir um valor maior do que a melhor solução existente, atualizamos a melhor
+            if node.integralSolution() and node.model.objective_value >= best_node.model.objective_value:
+                best_node = node
+
+            # escolhe a variavel pra ser ramificada (de valor fracionario mais proximo de 0,5)
+            variable_value_list = node.getVariableValueList()
+            branch_variable = getBranchVariableIndex(variable_value_list)
+            
+            # primeiro filho iguala essa variavel a 0
+            node.model += node.model.vars[branch_variable] == 0
+            tree.append(node)
+            print("\nnRestricoes do filho 1:")
+            for i in node.model.constrs:
+                print(i)
+
+            # segundo filho iguala essa variavel a 1
+            node.model.remove(node.model.constrs[-1])
+            node.model += node.model.vars[branch_variable] == 1
+            tree.append(node)
+            print("\nRestricoes do filho 2:")
+            for i in node.model.constrs:
+                print(i)
+        else:
+            # print(f"\nArvore antes da remocao: {tree}")
+            tree.remove(node)
+            # print(f"\nArvore depois da remocao: {tree}")
--- src/main.py
+++ src/main.py
@@ -1,184 +1,65 @@
-# importação do pacote mip
+from bnb import *
+
 from mip import *
 import numpy as np
 import sys
-# from bnb import BranchAndBound
-
-
-# salva modelo em arquivo lp, e mostra o conteúdo
-def save(model, filename):
-  model.write(filename) # salva modelo em arquivo
-  with open(filename, "r") as f: # lê e exibe conteúdo do arquivo
-    print(f.read())
 
 def solve(model):
-  status = model.optimize()
+    status = model.optimize()
 
-  if status != OptimizationStatus.OPTIMAL:
-    return
+    if status != OptimizationStatus.OPTIMAL:
+        return
 
-  print("Status = ", status)
-  print(f"Solution value  = {model.objective_value:.2f}\n")
-  
-  print("Solution:")
-  for v in model.vars:
-    print(f"{v.name} = {v.x:.2f}")
+    print("Status = ", status)
+    print(f"Solution value  = {model.objective_value:.2f}\n")
 
+    print("Solution:")
+    for v in model.vars:
+        print(f"{v.name} = {v.x:.2f}")
+    print("###########################################################")
 
-def getVariableValueList(model, variable_amount):
-  variable_list = []
-  for i in range(variable_amount):
-    variable_list.append(model.var_by_name(f"x_{i+1}").x)
-  return variable_list
-
-
-def Integer(variable):
-  if variable - int(variable) == 0:
-    return True
-  return False
-
-
-def proximity(variable):
-    return abs(variable - 0.5)
-
-
-def getBranchVariableIndex(variable_value_list):
-  # tira todos os valores inteiros e de valor 0 (temporariamente) da lista de valores das variáveis
-  value_list = [i for i in variable_value_list if i != 0 or not Integer(i)]
-  branch_variable = min(value_list, key=proximity)
-  return variable_value_list.index(branch_variable) # caso tenha mais de uma variável com o mesmo valor estamos escolhendo a de menor índice
-
+def save(model, filename):
+    model.write(filename) # salva modelo em arquivo
+    with open(filename, "r") as f: # lê e exibe conteúdo do arquivo
+        print(f.read())
 
 def main():
+    ################################
+    ####### Lendo a instancia ######
+    ################################
 
-  ################################
-  ####### Lendo a instancia ######
-  ################################
+    # file_path = "src/teste1.txt"
+    file_path = sys.argv[1]
+    f = open(file_path, "r")
+    
+    first_line  = f.readline().split()
+    funcao_obj = f.readline().split()
 
-  # file_path = "src/teste1.txt"
-  file_path = sys.argv[1]
-  f = open(file_path, "r")
-  
-  first_line  = f.readline().split()
-  funcao_obj = f.readline().split()
+    num_variaveis = int(first_line[0]) # numero de variaveis (variaveis enumeradas de 1 a n)
+    num_restricoes = int(first_line[1]) # número de restricoes (restricoes enumerados de 1 a n)
+    
+    restricoes = []
+    for i in range(num_restricoes):
+        restricoes.append(f.readline().split()) 
 
-  num_variaveis = int(first_line[0]) # numero de variaveis (variaveis enumeradas de 1 a n)
-  num_restricoes = int(first_line[1]) # número de restricoes (restricoes enumerados de 1 a n)
-  
-  restricoes = []
-  for i in range(num_restricoes):
-      restricoes.append(f.readline().split()) 
+    ################################
+    ####### Criando o modelo #######
+    ################################
 
-  ################################
-  ####### Criando o modelo #######
-  ################################
+    model = Model(name= "Branch and Bound", sense=MAXIMIZE, solver_name=CBC)
 
-  model = Model(name= "Branch and Bound", sense=MAXIMIZE, solver_name=CBC)
+    x = [model.add_var(var_type=CONTINUOUS, lb=0.0, name=f"x_{i+1}") for i in range(num_variaveis)]
+    
+    model.objective = xsum(int(funcao_obj[i]) * x[i] for i in range(len(funcao_obj)))
 
-  x = [model.add_var(var_type=CONTINUOUS, lb=0.0, name=f"x_{i+1}") for i in range(num_variaveis)]
-  
-  model.objective = xsum(int(funcao_obj[i]) * x[i] for i in range(len(funcao_obj)))
+    for i in range(num_restricoes):
+        model += xsum(int(restricoes[i][j]) * x[j] for j in range(num_variaveis)) <= int(restricoes[i][-1])
 
-  for i in range(num_restricoes):
-    model += xsum(int(restricoes[i][j]) * x[j] for j in range(num_variaveis)) <= int(restricoes[i][-1])
+    # save(model, "modelo1.lp")
+    # solve(model)
 
-  solve(model)
-  
-
-  list = getVariableValueList(model, num_variaveis)
-  branch_variable_index = getBranchVariableIndex(list)
-  model += x[branch_variable_index] == 1
-  solve(model)
-  
-  list = getVariableValueList(model, num_variaveis)
-  branch_variable_index = getBranchVariableIndex(list)
-  model += x[branch_variable_index] == 0
-  solve(model)
-  
-  list = getVariableValueList(model, num_variaveis)
-  branch_variable_index = getBranchVariableIndex(list)
-  model += x[branch_variable_index] == 0
-  solve(model)
-
-  save(model, "modelo1.lp")
-
-
-  ################################
-  ########## Algoritmo ###########
-  ################################
-
-  # model += x[1] == 1
-
-  # model += x[2] == 0
-
-  # model += x[0] == 0
-
-  # save(model, "modelo1.lp")
-  # solve(model)
-  
-#   Node BranchBound_DFS(Data *data, double **cost){ // executa o algoritmo utilizando busca por profundidade DFS(Depth First Search)
-# 	Node root; // cria o nó raíz da árvore
-#     Node best_solution; // cria o nó em que será guardada a melhor solução
-# 	best_solution.cost = numeric_limits<double>::infinity(); // seta o custo da melhor solução inicialmente para "infinito"
-# 	list<Node> tree;
-# 	tree.push_back(root); // cria a árvore que será preenchida e feita a busca nela
-#     // a "árvore" será na verdade uma lista em que iremos inserindo e removendo no começo e/ou no final de acordo com o algortimo
-
-# 	double upper_bound = 99999999; // "qualquer", ou seja, o primeiro upper_bound encontrado já será atribuído a essa variável devido ao seu custo
-
-# 	while(!tree.empty()){ // equanto todos os nós da árvore não forem analisados
-#     	// auto node = tree.end(); // assim não funciona, precisa ser assim aí na linha de baixo
-#     	auto node = prev(tree.end()); // vai pelo último nó -> DFS strategy
-
-# 		getSolutionHungarian(*node, data); // calcula a solução do subtour atual
-
-#         // se for debugar descomenta a linha de baixo, mas o melhor pra debuggar é logo aqui embaixo na linha 231
-#         // show_info(*node);
-
-# 		if(node->cost > upper_bound){ // solução gerada pelo algoritmo húngaro do assignment problema é descartada pois o custo é maior que o upper bound
-# 			tree.erase(node);
-# 			continue;
-# 		}
-# 		if(node->feasible == true){ // se a solução for viável
-# 			if(node->cost < best_solution.cost){// e se o custo da solução for menor que o o custo da melhor solução encontrada até agora
-#                 best_solution = *node; // atualizamos a melhor solução
-# 				upper_bound = node->cost; // upper_bound é atualizado com o custo dessa nova solução
-#                 // Solução viável foi gerada, caso queira olhar as soluções geradas na execução do algoritmo descomenta "show_info()"
-#                 // show_info(best_solution);
-# 			}
-
-# 			tree.erase(node); // esse nó já foi analisado então tira ele da árvore
-# 			continue;
-# 		}
-#         // Solução inviável foi gerada, caso queira olhar as soluções geradas na execução do algoritmo descomenta "show_info()"
-#         // else{
-#         //         // show_info(node);
-#         // }
-        
-# 	# aqui tava dando mt problema pq o (node.subtours[i].size()) nunca era < que (node.subtours[0].size()) no final da função getSolutionHungarian() então
-# 	# o node->pick não era escolhido, fazendo com que ele ficasse com o valor inicializado, e eu não setava ele inicialmente pra 0, então ele pegava lixo
-# 	# de memória. Logo, esse loop aqui de baixo nunca era inicializado e bugava o código. Pra arrumar só seto o pick inicial pra zero, ou seja, se ele não
-# 	# achar nenhum tour menor pra escolher, (todos são do mesmo tamanho), ele escolhe o primeiro.
-#   for(int i = 0; i < node->subtours[node->pick].size() - 1; i++){ // Adiciona folhas/filhos na árvore
-#     Node new_node;
-#     new_node.forbidden_arcs = node->forbidden_arcs; // atribui os arcos proibidos do nó da árvore ao nó atual que será inserido no final da árvore
-
-#     pair<int, int> new_forbidden_arc;
-#     new_forbidden_arc.first = node->subtours[node->pick][i];
-#     new_forbidden_arc.second = node->subtours[node->pick][i + 1];
-
-#     new_node.forbidden_arcs.push_back(new_forbidden_arc);
-#     tree.insert(tree.end(), new_node); // insere novos nós na árvore de busca
-#   }
-
-#   tree.erase(node);
-# }
-
-#     return best_solution; 
-# }
+    solveProblem(model)
 
 if __name__ == "__main__":
-  # bnb = BranchAndBound()
-  # bnb.solve()
   main()
 # soluções ótimas do teobaldo: 20, 24, 19 e 10
